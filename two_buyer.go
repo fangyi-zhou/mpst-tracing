5ec070061829e3d2879a09eb46a205e5650aa63b
Fangyi Zhou <me@fangyi.io>
2020-09-16 17:53:02 +0100

    Go fmt

--- two_buyer.go
+++ two_buyer.go
@@ -5,30 +5,30 @@ import (
 	"sync"
 )
 
-type A struct {}
-type B struct {}
-type C struct {}
+type A struct{}
+type B struct{}
+type C struct{}
 
-func (*A) run (wg *sync.WaitGroup, b *B, c *C) {
+func (*A) run(wg *sync.WaitGroup, b *B, c *C) {
 	defer wg.Done()
 	fmt.Println("Running A")
 }
 
-func (*B) run (wg *sync.WaitGroup, a *A, c *C) {
+func (*B) run(wg *sync.WaitGroup, a *A, c *C) {
 	defer wg.Done()
 	fmt.Println("Running B")
 }
 
-func (*C) run (wg *sync.WaitGroup, a *A, b *B) {
+func (*C) run(wg *sync.WaitGroup, a *A, b *B) {
 	defer wg.Done()
 	fmt.Println("Running C")
 }
 
 func runAll() {
 	var wg sync.WaitGroup
-	var a = A {}
-	var b = B {}
-	var c = C {}
+	var a = A{}
+	var b = B{}
+	var c = C{}
 	wg.Add(3)
 	go a.run(&wg, &b, &c)
 	go b.run(&wg, &a, &c)
