aa8058082473d12c9813ccfe518a2198b158e418
Fangyi Zhou <me@fangyi.io>
2020-09-30 15:35:11 +0100

    Separate some boilerplate code in a separate repo

--- two_buyer.go
+++ two_buyer.go
@@ -6,49 +6,6 @@ import (
 	"sync"
 )
 
-type A struct {
-	ba chan int
-	ca chan int
-	b  *B
-	c  *C
-}
-type B struct {
-	ab chan int
-	cb chan int
-	a  *A
-	c  *C
-}
-type C struct {
-	ac chan int
-	bc chan int
-	a  *A
-	b  *B
-}
-
-func (a *A) sendB(v int) {
-	a.b.ab <- v
-}
-
-func (a *A) sendC(v int) {
-	a.c.ac <- v
-}
-
-func (b *B) sendA(v int) {
-	b.a.ba <- v
-}
-
-func (b *B) sendC(v int) {
-	b.c.bc <- v
-}
-
-func (c *C) sendA(v int) {
-	c.a.ca <- v
-}
-
-func (c *C) sendB(v int) {
-	c.b.cb <- v
-}
-
 func (a *A) run(wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println("Running A")
