74fdc6aea1da244118f3990437c90e2e55afdb05
Fangyi Zhou <me@fangyi.io>
2020-10-07 23:14:20 +0100

    Print service name

--- processors/mpstconformancecheckingprocessor/processor.go
+++ processors/mpstconformancecheckingprocessor/processor.go
@@ -4,16 +4,29 @@ import (
 	"context"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/pdata"
+	"go.opentelemetry.io/collector/processor"
 	"go.uber.org/zap"
 )
 
 type mpstConformanceProcessor struct {
+	logger *zap.Logger
 }
 
 func (m mpstConformanceProcessor) ProcessTraces(ctx context.Context, traces pdata.Traces) (pdata.Traces, error) {
-	panic("implement me")
+	spans := traces.ResourceSpans()
+	for i := 0; i < spans.Len(); i++ {
+		span := spans.At(i)
+		if span.IsNil() {
+			continue
+		}
+		serviceName := processor.ServiceNameForResource(span.Resource())
+		m.logger.Info("Found trace for service", zap.String("serviceName", serviceName))
+	}
+	return traces, nil
 }
 
 func newMpstConformanceProcessor(logger *zap.Logger, nextConsumer consumer.TraceConsumer, cfg *Config) (mpstConformanceProcessor, error) {
-	return mpstConformanceProcessor{}, nil
+	return mpstConformanceProcessor{
+		logger: logger,
+	}, nil
 }
