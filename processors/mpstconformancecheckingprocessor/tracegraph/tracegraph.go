129b1cb8f2224cee5f3f1ecc1a266992c9da9236
Fangyi Zhou <me@fangyi.io>
2020-10-29 22:51:23 +0000

    Add send/recv dependency edges

--- processors/mpstconformancecheckingprocessor/tracegraph/tracegraph.go
+++ processors/mpstconformancecheckingprocessor/tracegraph/tracegraph.go
@@ -14,6 +14,20 @@ type Message struct {
 	Action string
 }
 
+func (m Message) toIndex() messageIndex {
+	return messageIndex{
+		label: m.Label,
+		origin: m.Origin,
+		dest: m.Dest,
+	}
+}
+
+type messageIndex struct {
+	label string
+	origin string
+	dest string
+}
+
 type LocalTrace []Message
 
 type TraceGraph struct {
@@ -32,7 +46,7 @@ func (n graphNode) ID() int64 {
 
 func (n graphNode) DOTID() string {
 	message := n.graph.items[n.idx]
-	return fmt.Sprintf("%s %s %s: %s", message.Origin, message.Action, message.Dest, message.Label)
+	return fmt.Sprintf("%s_%s_%s:%s", message.Origin, message.Action, message.Dest, message.Label)
 }
 
 func makeNode(traceGraph *TraceGraph, id int64) graphNode {
@@ -44,22 +58,51 @@ func Construct(traces map[string]LocalTrace) TraceGraph {
 	var idx int64 = 0
 	var traceGraph TraceGraph
 	var items = make([]Message, 0)
+	var sendBuffer = map[messageIndex][]int64{}
+	var recvBuffer = map[messageIndex][]int64{}
 	for _, localTrace := range traces {
-		items = append(items, localTrace[0])
-		msgGraph.AddNode(makeNode(&traceGraph, idx))
-		idx ++
-		if len(localTrace) > 1 {
-			for i := 1; i < len(localTrace); i++ {
-				items = append(items, localTrace[i])
-				msgGraph.AddNode(makeNode(&traceGraph, idx))
+		for i, msg := range localTrace {
+			items = append(items, msg)
+			msgGraph.AddNode(makeNode(&traceGraph, idx))
+			if i > 0 {
+				// Local Sequencing
 				edge := msgGraph.NewEdge(makeNode(&traceGraph, idx-1), makeNode(&traceGraph, idx))
 				msgGraph.SetEdge(edge)
-				idx ++
 			}
+			mIdx := msg.toIndex()
+			if msg.Action == "send" {
+				if buf, exists := recvBuffer[mIdx]; exists {
+					rIdx := buf[0]
+					recvBuffer[mIdx] = buf[1:]
+					edge := msgGraph.NewEdge(makeNode(&traceGraph, idx), makeNode(&traceGraph, rIdx))
+					msgGraph.SetEdge(edge)
+				} else {
+					if buf, exists := sendBuffer[mIdx]; exists {
+						sendBuffer[mIdx] = append(buf, idx)
+					} else {
+						sendBuffer[mIdx] = []int64{idx}
+					}
+				}
+			} else if msg.Action == "recv" {
+				if buf, exists := sendBuffer[mIdx]; exists {
+					sIdx := buf[0]
+					sendBuffer[mIdx] = buf[1:]
+					edge := msgGraph.NewEdge(makeNode(&traceGraph, sIdx), makeNode(&traceGraph, idx))
+					msgGraph.SetEdge(edge)
+				} else {
+					if buf, exists := recvBuffer[mIdx]; exists {
+						recvBuffer[mIdx] = append(buf, idx)
+					} else {
+						recvBuffer[mIdx] = []int64{idx}
+					}
+				}
+			} else {
+				panic("unknown action " + msg.Action)
+			}
+			idx ++
 		}
 	}
 	traceGraph.items = items
-	// TODO: Add Send/Recv Edges
 	// TODO: Remove debug printings
 	dotGraph, err := dot.Marshal(msgGraph, "Messages", "", "  ")
 	if err != nil {
