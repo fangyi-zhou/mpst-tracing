40fe1423d1944cb30545b39410b0570f3ca6db31
Fangyi Zhou <me@fangyi.io>
2020-10-30 15:02:53 +0000

    Add TwoBuyer Global Type

--- processors/mpstconformancecheckingprocessor/globaltype/gtype.go
+++ processors/mpstconformancecheckingprocessor/globaltype/gtype.go
@@ -14,3 +14,62 @@ type GlobalType interface {
 func Parse(input string) (GlobalType, error) {
 	return nil, errors.New("unimplemented: parse")
 }
+
+func TwoBuyer() GlobalType {
+	return Send{
+		origin: "A",
+		dest:   "B",
+		conts: map[string]GlobalType{
+			"query": Recv{
+				origin: "A",
+				dest:   "B",
+				label:  "query",
+				cont:   Send{
+					origin: "B",
+					dest:   "A",
+					conts: map[string]GlobalType{
+						"quote": Recv{
+							origin: "B",
+							dest:   "A",
+							label:  "quote",
+							cont:   Send{
+								origin: "B",
+								dest:   "C",
+								conts: map[string]GlobalType{
+									"quote": Recv{
+										origin: "B",
+										dest:   "C",
+										label:  "quote",
+										cont:  Send{
+											origin: "C",
+											dest:   "A",
+											conts: map[string]GlobalType{
+												"share": Recv{
+													origin: "C",
+													dest:   "A",
+													label:  "share",
+													cont:  Send{
+														origin: "A",
+														dest:   "B",
+														conts: map[string]GlobalType{
+															"buy": Recv{
+																origin: "A",
+																dest:   "B",
+																label:  "buy",
+																cont:   Done{},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
