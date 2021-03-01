6d6e7bbf809a25288536765c926462ecb52e9e0e
Fangyi Zhou <me@fangyi.io>
2021-03-01 17:21:24 +0000

    Add some initial types for Petri net

--- pedro/pedro.go
+++ pedro/pedro.go
@@ -1,1 +1,41 @@
 package pedro
+
+import "errors"
+
+type PetriNet interface{}
+
+type petriNet struct {
+	tokens              []token
+	placesOrTransitions []placeOrTransition
+	arcs                []arc
+}
+
+type arc struct {
+	source      placeOrTransition
+	destination placeOrTransition
+	tokens      []token
+}
+
+type token string
+type placeOrTransition string
+type label string
+
+type tokenWithMultiplicity struct {
+	token        token
+	multiplicity int
+}
+
+type marking map[placeOrTransition]tokenWithMultiplicity
+
+type MarkedPetriNet struct {
+	pn      PetriNet
+	marking marking
+}
+
+func LoadFromSexpFile(filename string) (error, *MarkedPetriNet) {
+	return errors.New("I didn't implement"), nil
+}
+
+func (m *MarkedPetriNet) Reduce(label label) error {
+	return errors.New("I didn't implement")
+}
