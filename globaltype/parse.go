3ec2459964d37e12a82cfa7d9a5d7cc461096789
Fangyi Zhou <me@fangyi.io>
2021-03-09 17:35:00 +0000

    Implement parsing for choices

--- globaltype/parse.go
+++ globaltype/parse.go
@@ -26,9 +26,57 @@ func loadGType(node *sexp.Node) (GlobalType, error) {
 	switch node.Children.Value {
 	case "MessageG":
 		return loadMessageG(node.Children)
+	case "ChoiceG":
+		return loadChoiceG(node.Children)
 	default:
 		return nil, fmt.Errorf("unrecognised global type constructor %s", node.Children.Value)
 	}
+}
+
+func loadChoiceG(node *sexp.Node) (GlobalType, error) {
+	// node is the keyword ChoiceG
+	// the next s-expression is the choice role
+
+	// The list of continuations follows
+	choices := node.Next.Next
+	if choices == nil || choices.IsScalar() {
+		return nil, fmt.Errorf("Expect a list of choices")
+	}
+	choice := choices.Children
+	gtypes := make([]GlobalType, 0)
+	for choice != nil {
+		gtype, err := loadGType(choice)
+		if err != nil {
+			return nil, err
+		}
+		gtypes = append(gtypes, gtype)
+		choice = choice.Next
+	}
+	return combineGtypes(gtypes)
+}
+
+func combineGtypes(gtypes []GlobalType) (GlobalType, error) {
+	if len(gtypes) == 0 {
+		// nuscr should probably report an error of an empty choice, but we handle this case for completeness
+		return NewDone(), nil
+	} else if len(gtypes) == 1 {
+		// nuscr should probably also report an error in the case of a degenerate choice, we also handle for completeness
+		return gtypes[0], nil
+	}
+	combined := Send{conts: make(map[string]GlobalType)}
+	for _, gtype := range gtypes {
+		switch gtype := gtype.(type) {
+		case Send:
+			combined.origin = gtype.origin
+			combined.dest = gtype.dest
+			for label, k := range gtype.conts {
+				combined.conts[label] = k
+			}
+		default:
+			return nil, fmt.Errorf("cannot combine gtypes %s", gtype)
+		}
+	}
+	return combined, nil
 }
 
 func loadMessageG(node *sexp.Node) (GlobalType, error) {
--- twobuyer/TwoBuyer_global_type.sexp
+++ twobuyer/TwoBuyer_global_type.sexp
@@ -2,4 +2,6 @@
  (MessageG ((label quote) (payload ())) S A
   (MessageG ((label quote) (payload ())) S B
    (MessageG ((label share) (payload ())) B A
-    (MessageG ((label buy) (payload ())) A S EndG)))))
+    (ChoiceG A
+     ((MessageG ((label buy) (payload ())) A S EndG)
+      (MessageG ((label cancel) (payload ())) A S EndG)))))))
