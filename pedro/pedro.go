7274cb513ff7c3fa03e9bf6cc201e22887f788da
Francisco Ferreira <fferreira@gmail.com>
2021-03-10 13:27:33 +0000

    new representation

--- pedro/pedro.go
+++ pedro/pedro.go
@@ -1,7 +1,5 @@
 package pedro
 
-import "errors"
-
 type PetriNet struct {
 	tokens              []token
 	placesOrTransitions []label // these may not be a great idea, some transitions need to be marked as silent
@@ -11,18 +9,22 @@ type PetriNet struct {
 type arc struct {
 	source      label
 	destination label
-	tokens      []tokenWithMultiplicity //These should be fifos of tokens, no multiplicity, has to be changed in pedro(OCaml)
+	tokens      []entityMarking //These should be fifos of tokens, no multiplicity, has to be changed in pedro(OCaml)
 }
 
 type token string
 type label string // the label of a place or a transition
 
-type tokenWithMultiplicity struct {
+/*type tokenWithMultiplicity struct {
 	token        token
 	multiplicity int
-}
+}*/
 
-type marking map[label][]tokenWithMultiplicity
+type tokenQueue = [] token
+
+type entityMarking = map[label] tokenQueue
+
+type marking map[label]entityMarking
 
 type MarkedPetriNet struct {
 	pn      PetriNet
@@ -78,94 +80,82 @@ func tokenSplit(a []token, b []token) ([]token, []token, []token) {
 
 // consumes from a the tokens in b, returns first the extra resources that a has, the consumed resources, and the resources from b that remain to be provided
 // this function is a bit scary
-func consumeResources(a []tokenWithMultiplicity, b []tokenWithMultiplicity) ([]tokenWithMultiplicity, []tokenWithMultiplicity, []tokenWithMultiplicity) {
-	var extra []tokenWithMultiplicity
-	var consumed []tokenWithMultiplicity
-	var remaining []tokenWithMultiplicity
+//func consumeResources (a []tokenWithMultiplicity, b []tokenWithMultiplicity) ([]tokenWithMultiplicity, []tokenWithMultiplicity, []tokenWithMultiplicity){
+//	var extra []tokenWithMultiplicity
+//	var consumed []tokenWithMultiplicity
+//	var remaining []tokenWithMultiplicity
+//
+//	for _,ael := range a {
+//		toConsume := 0 // flag for in a and not in b
+//		for _, bel := range b {
+//			if ael.token == bel.token { toConsume = bel.multiplicity ; break }
+//		}
+//		if toConsume > 0 {
+//
+//			if ael.multiplicity > toConsume { //if there are strictly more tokens than required
+//				eel := ael ; eel.multiplicity = ael.multiplicity - toConsume
+//				extra = append(extra, eel)
+//
+//				cel := ael ; cel.multiplicity = toConsume
+//				consumed = append(consumed, cel)
+//			} else if ael.multiplicity == toConsume {
+//				consumed = append(consumed, ael) // all resources were consumed
+//			} else {
+//				cel := ael ; cel.multiplicity = ael.multiplicity
+//				consumed = append(consumed, cel)
+//
+//				rel := ael ; cel.multiplicity = toConsume - ael.multiplicity
+//				remaining = append(remaining, rel)
+//			}
+//		} else {
+//			extra = append(extra, ael)
+//		}
+//	}
+//	for _,bel := range b {
+//		stillRemain := true
+//		for _, ael := range a {
+//			if ael.token == bel.token { stillRemain = false ; break}
+//		}
+//		if stillRemain { remaining = append(remaining, bel) }
+//	}
+//	return extra, consumed, remaining
+//}
 
-	for _, ael := range a {
-		toConsume := 0 // flag for in a and not in b
-		for _, bel := range b {
-			if ael.token == bel.token {
-				toConsume = bel.multiplicity
-				break
-			}
-		}
-		if toConsume > 0 {
+//func mem(tk tokenWithMultiplicity, tks []tokenWithMultiplicity) (bool, *tokenWithMultiplicity) {
+//	for _,v := range tks {
+//		if tk.token == v.token { return true, nil}
+//	}
+//
+//	return false, nil
+//}
+//
+//// collects the silent transitions that may bring
+//func (m MarkedPetriNet) collectSilentTransition (dst label, tk tokenWithMultiplicity){
+//	for _, a := range m.pn.arcs {
+//		if a.destination == dst && a.source == "" { //this is an arc of interest (is this how we know if it is silent?)
+//			//var xx = mem(tk, a.tokens)
+//
+//
+//		}
+//	}
+//
+//}
 
-			if ael.multiplicity > toConsume { //if there are strictly more tokens than required
-				eel := ael
-				eel.multiplicity = ael.multiplicity - toConsume
-				extra = append(extra, eel)
-
-				cel := ael
-				cel.multiplicity = toConsume
-				consumed = append(consumed, cel)
-			} else if ael.multiplicity == toConsume {
-				consumed = append(consumed, ael) // all resources were consumed
-			} else {
-				cel := ael
-				cel.multiplicity = ael.multiplicity
-				consumed = append(consumed, cel)
-
-				rel := ael
-				cel.multiplicity = toConsume - ael.multiplicity
-				remaining = append(remaining, rel)
-			}
-		} else {
-			extra = append(extra, ael)
-		}
-	}
-	for _, bel := range b {
-		stillRemain := true
-		for _, ael := range a {
-			if ael.token == bel.token {
-				stillRemain = false
-				break
-			}
-		}
-		if stillRemain {
-			remaining = append(remaining, bel)
-		}
-	}
-	return extra, consumed, remaining
-}
-
-func mem(tk tokenWithMultiplicity, tks []tokenWithMultiplicity) (bool, *tokenWithMultiplicity) {
-	for _, v := range tks {
-		if tk.token == v.token {
-			return true, nil
-		}
-	}
-
-	return false, nil
-}
-
-// collects the silent transitions that may bring
-func (m MarkedPetriNet) collectSilentTransition(dst label, tk tokenWithMultiplicity) {
-	for _, a := range m.pn.arcs {
-		if a.destination == dst && a.source == "" { //this is an arc of interest (is this how we know if it is silent?)
-			//var xx = mem(tk, a.tokens)
-
-		}
-	}
-
-}
-
-func (m MarkedPetriNet) consumeResourcesOnMarking(act []arc) (error, []arc) {
-	mark := m.marking // copy the marking (hopefully, I don't undertand Go semantics well enough)
-
-	for _, v := range act {
-		var extra, _, _ = /* remaining */ consumeResources(mark[v.source], v.tokens)
-		mark[v.source] = extra //resources that stay in the marking after firing
-
-		// at this point we have to process the remaining tokens to see if we can get them all.
-
-	}
-	return errors.New("I didn't implement"), nil
-}
-
-func (m MarkedPetriNet) Reduce(label label) (error, marking) {
-
-	return errors.New("I didn't implement"), nil
-}
+//func (m MarkedPetriNet) consumeResourcesOnMarking(act []arc) (error, []arc){
+//	mark := m.marking // copy the marking (hopefully, I don't undertand Go semantics well enough)
+//
+//	for _,v := range act {
+//		var extra, _, _ /* remaining */ = consumeResources(mark[v.source], v.tokens)
+//		mark[v.source] = extra //resources that stay in the marking after firing
+//
+//		// at this point we have to process the remaining tokens to see if we can get them all.
+//
+//	}
+//return errors.New("I didn't implement"), nil
+//}
+//
+//
+//func (m MarkedPetriNet) Reduce(label label) (error, marking) {
+//
+//	return errors.New("I didn't implement"), nil
+//}
