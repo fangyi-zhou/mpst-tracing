980d93ead0a79594103bedb0e69822396fc9a388
Fangyi Zhou <me@fangyi.io>
2021-03-30 14:18:04 +0100

    Add some logging

--- semanticmodel/model/model.go
+++ semanticmodel/model/model.go
@@ -1,5 +1,7 @@
 package model
 
+import "go.uber.org/zap"
+
 type SemanticModel interface {
 	IsTerminated() bool
 	TryReduce(action Action) bool
@@ -8,15 +10,21 @@ type SemanticModel interface {
 
 type Model struct {
 	SemanticModel
+	logger *zap.Logger
 	traces map[string][]Action
 }
 
 func MakeModel(semanticModel SemanticModel) Model {
-	return Model{SemanticModel: semanticModel, traces: make(map[string][]Action)}
+	return Model{SemanticModel: semanticModel, traces: make(map[string][]Action), logger: zap.NewNop()}
+}
+
+func MakeModelWithLogger(semanticModel SemanticModel, logger *zap.Logger) Model {
+	return Model{SemanticModel: semanticModel, traces: make(map[string][]Action), logger: logger}
 }
 
 func (m *Model) AcceptTrace(participant string, traces []Action) {
 	m.traces[participant] = append(m.traces[participant], traces...)
+	m.logger.Info("AcceptTrace", zap.String("participant", participant), zap.Int("number", len(traces)))
 	m.processTraces()
 }
 
