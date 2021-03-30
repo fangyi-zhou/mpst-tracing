1534a6cc403840219a8e324f24ffb552b5600b39
Fangyi Zhou <me@fangyi.io>
2021-03-30 23:15:28 +0100

    Implement processtrace

--- semanticmodel/model/model.go
+++ semanticmodel/model/model.go
@@ -10,16 +10,17 @@ type SemanticModel interface {
 
 type Model struct {
 	SemanticModel
-	logger *zap.Logger
-	traces map[string][]Action
+	logger  *zap.Logger
+	traces  map[string][]Action
+	isStuck bool
 }
 
 func MakeModel(semanticModel SemanticModel) Model {
-	return Model{SemanticModel: semanticModel, traces: make(map[string][]Action), logger: zap.NewNop()}
+	return Model{SemanticModel: semanticModel, traces: make(map[string][]Action), logger: zap.NewNop(), isStuck: false}
 }
 
 func MakeModelWithLogger(semanticModel SemanticModel, logger *zap.Logger) Model {
-	return Model{SemanticModel: semanticModel, traces: make(map[string][]Action), logger: logger}
+	return Model{SemanticModel: semanticModel, traces: make(map[string][]Action), logger: logger, isStuck: false}
 }
 
 func (m *Model) AcceptTrace(participant string, traces []Action) {
@@ -30,5 +31,32 @@ func (m *Model) AcceptTrace(participant string, traces []Action) {
 
 func (m *Model) processTraces() {
 	m.logger.Info("Processing Traces")
-	// TODO
+	allHaveData := false
+	for {
+		allHaveData = true
+		reduced := false
+		for participant, trace := range m.traces {
+			if len(trace) == 0 {
+				allHaveData = false
+				continue
+			}
+			action := trace[0]
+			if m.TryReduce(action) {
+				m.logger.Info("Action reduced successfully", zap.String("action", action.String()))
+				m.traces[participant] = m.traces[participant][1:]
+				reduced = true
+			}
+		}
+		if !reduced {
+			break
+		}
+	}
+	if allHaveData {
+		m.logger.Error("Model is stuck")
+		m.isStuck = true
+	}
+}
+
+func (m *Model) IsStuck() bool {
+	return m.isStuck
 }
