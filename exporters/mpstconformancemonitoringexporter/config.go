2ee61c24a65e93e57bacf16eec0dcf46302c8f4f
Fangyi Zhou <me@fangyi.io>
2021-03-31 14:38:12 +0100

    Add config field for pedrolib path

--- exporters/mpstconformancemonitoringexporter/config.go
+++ exporters/mpstconformancemonitoringexporter/config.go
@@ -13,4 +13,6 @@ type Config struct {
 	ProtocolFileName string `mapstructure:"protocol_filename"`
 	//ProtocolName is the name of global protocol in file specified at ProtocolFileName
 	ProtocolName string `mapstructure:"protocol_name"`
+	//PedroSoFileName is the path to plibpedroedrolib.so
+	PedroSoFileName string `mapstructure:"pedro_so_filename"`
 }
--- exporters/mpstconformancemonitoringexporter/config_test.go
+++ exporters/mpstconformancemonitoringexporter/config_test.go
@@ -10,7 +10,7 @@ import (
 	"go.opentelemetry.io/collector/config/configtest"
 )
 
-func TestLoadConfig(t *testing.T) {
+func TestLoadConfigLts(t *testing.T) {
 	factories, err := componenttest.ExampleComponents()
 	assert.NoError(t, err)
 
@@ -20,7 +20,24 @@ func TestLoadConfig(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, cfg)
 
-	mpstConfig := cfg.Exporters["mpstconformancemonitoring"].(*Config)
+	mpstConfig := cfg.Exporters["mpstconformancemonitoring/lts"].(*Config)
 	assert.Equal(t, "gtype_lts", mpstConfig.SemanticModelType)
 	assert.Equal(t, "gtype.sexp", mpstConfig.GlobalTypeSexpFileName)
+}
+
+func TestLoadConfigPedro(t *testing.T) {
+	factories, err := componenttest.ExampleComponents()
+	assert.NoError(t, err)
+
+	factories.Exporters[typeStr] = NewFactory()
+
+	cfg, err := configtest.LoadConfigFile(t, path.Join(".", "testdata", "config.yaml"), factories)
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	mpstConfig := cfg.Exporters["mpstconformancemonitoring/pedro"].(*Config)
+	assert.Equal(t, "gtype_pedro", mpstConfig.SemanticModelType)
+	assert.Equal(t, "pedrolib.so", mpstConfig.PedroSoFileName)
+	assert.Equal(t, "MyProto.nuscr", mpstConfig.ProtocolFileName)
+	assert.Equal(t, "MyProto", mpstConfig.ProtocolName)
 }
