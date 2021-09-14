04eb80c2aa9a1b395219a80104dddf465f623a4c
Fangyi Zhou <me@fangyi.io>
2021-09-14 10:34:06 +0100

    Update Pedro version

--- exporters/mpstconformancemonitoringexporter/semanticmodel/pedro/version.h
+++ exporters/mpstconformancemonitoringexporter/semanticmodel/pedro/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PEDRO_API_HASH "e914f3d64f1b51e4d406545f0b4ea5412d003284"
+#define PEDRO_API_HASH "dc59e984913fc93ed9bbd74117649f63ef678ae6"
 
 #endif // VERSION_H
