7e1d46caac522d326151a475fe10123f7cfd2bdf
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2022-04-29 14:12:24 +0100

    Throw away some unused code in comments

--- processors/mpstconformancemonitoringprocessor/processor.go
+++ processors/mpstconformancemonitoringprocessor/processor.go
@@ -123,77 +123,7 @@ func (m mpstConformanceMonitoringProcessor) ConsumeTraces(
 		return err
 	}
 	return m.nextConsumer.ConsumeTraces(ctx, td)
-	/*
-		err := checkSendRecvMatching(localTraces)
-		if err != nil {
-			return err
-		}
-		causalOrder := causalorder.Construct(m.logger, localTraces)
-		err = causalOrder.CheckProtocolConformance(m.gtype)
-		return err
-	*/
 }
-
-/*
-type participantPair struct {
-	from string
-	to   string
-}
-
-func checkSendRecvMatching(traces map[string]causalorder.LocalTrace) error {
-	sendQueues := map[participantPair][]globaltype.Message{}
-	recvQueues := map[participantPair][]globaltype.Message{}
-	var errs []error
-	for endpoint, localTrace := range traces {
-		for _, message := range localTrace {
-			if message.Action == "send" {
-				sendQueues[participantPair{endpoint, message.Dest}] = append(sendQueues[participantPair{endpoint, message.Dest}], message)
-			} else {
-				recvQueues[participantPair{message.Origin, endpoint}] = append(recvQueues[participantPair{message.Origin, endpoint}], message)
-			}
-		}
-	}
-	for ppair, sendQueue := range sendQueues {
-		for _, sendMsg := range sendQueue {
-			if len(recvQueues[ppair]) == 0 {
-				errs = append(errs, missingRecvMessageErr(ppair.from, ppair.to, sendMsg))
-				continue
-			}
-			recvMsg := recvQueues[ppair][0]
-			if sendMsg.Label != recvMsg.Label {
-				errs = append(errs, mismatchLabelErr(ppair.from, ppair.to, sendMsg, recvMsg))
-				continue
-			}
-			recvQueues[ppair] = recvQueues[ppair][1:]
-		}
-	}
-
-	for ppair, recvQueue := range recvQueues {
-		for _, recvMsg := range recvQueue {
-			errs = append(errs, recvWithoutSendErr(ppair.from, ppair.to, recvMsg))
-		}
-	}
-
-	if len(errs) != 0 {
-		return componenterror.CombineErrors(errs)
-	}
-	return nil
-}
-*/
-
-/*
-func recvWithoutSendErr(orig string, dest string, msg globaltype.Message) error {
-	return fmt.Errorf("message labelled %s received by %s without matching send, allegedly from %s", msg.Label, dest, orig)
-}
-
-func mismatchLabelErr(orig string, dest string, sendMsg globaltype.Message, recvMsg globaltype.Message) error {
-	return fmt.Errorf("message label mismatch, sent from %s to %s, label %s is sent, but %s is received", orig, dest, sendMsg.Label, recvMsg.Label)
-}
-
-func missingRecvMessageErr(orig string, dest string, msg globaltype.Message) error {
-	return fmt.Errorf("message labelled %s sent from %s is not received by %s", msg.Label, orig, dest)
-}
-*/
 
 func newMpstConformanceProcessor(
 	logger *zap.Logger,
